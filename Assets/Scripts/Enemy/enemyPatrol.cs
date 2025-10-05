5c9b1f1ec2f3cb567e5e1c7d60ac27f6c1519b8e
Shubhodeep <97751363+shubhz2003@users.noreply.github.com>
2025-10-05 18:05:39 -0400

    simple Patrolling enemy

---
+++ Assets/Scripts/Enemy/enemyPatrol.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class enemyPatrol : MonoBehaviour
+{
+    public GameObject PointA;
+    public GameObject PointB;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+    public float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = PointB.transform;
+        anim.SetBool("isPatrolling", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == PointB.transform)
+        {
+            rb.linearVelocity = new Vector2(speed, 0);
+        }
+        else
+        { 
+            rb.linearVelocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointB.transform)
+        {
+            currentPoint = PointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == PointA.transform)
+        {
+            currentPoint = PointB.transform;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(PointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(PointB.transform.position, 0.5f);
+        Gizmos.DrawLine(PointA.transform.position, PointB.transform.position);
+    }
+}
